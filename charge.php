26f93ea559338bc21abbb14c2b72f1e9d0c62a96
Johnny Le <lejohnn2@gmail.com>
2018-03-26 16:48:23 -0400

    Commit

--- charge.php
+++ charge.php
@@ -1,5 +1,11 @@
 <?php
+ require_once('vendor/autoload.php');
+ require_once('config/db.php');
+ require_once('lib/pdo_db.php');
+ require_once('models/Customer.php');
+ require_once('models/Transaction.php');
 
+ \Stripe\Stripe::setApiKey('sk_test_Gkmiw7YpMZLodhr0Ab1WOO9g');
 
 
 ?>
